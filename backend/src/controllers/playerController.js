efeb4e632d4db5b02268e6b81fa36d9d84603ada
Navah-k12 <navahca21@gmail.com>
2026-09-13 23:31:51 -0500

    feat: Se realizo cambios para conectar con la tabla en la bd

--- backend/src/controllers/playerController.js
+++ backend/src/controllers/playerController.js
@@ -1,43 +1,43 @@
 
 
-const db = require('../config/db');
+    const db = require('../config/db');
 
-const registerPlayers = async (req,res) => {
-    const {name, gamertag, email}= req.body;
+    const registerPlayers = async (req,res) => {
+        const {nombre, gamertag, correo}= req.body;
 
-    if(!name || !gamertag || !email)
-    {
-        res.status(400).json({error: 'Error: No deje ni un campo vacio'})
-        return;
+        if(!nombre || !gamertag || !correo)
+        {
+            res.status(400).json({error: 'Error: No deje ni un campo vacio'})
+            return;
+        };
+
+        try{
+            const query ='INSERT INTO jugadores(nombre, gamertag, correo) VALUES (?,?,?)'
+            await db.query(query, [nombre, gamertag, correo]);
+
+            return res.status(201).json({message: 'Jugador registrado exitosamente'});
+        }catch(error){
+            console.error(error);
+            
+
+            if(error.code === 'ER_DUP_ENTRY'){
+                return res.status(400).json({error: 'El Gamertag o el Email no se encuentra disponible'});
+            }
+
+            res.status(500).json({error:'Error en el servidor al registrarte'});
+        };
     };
 
-    try{
-        const query ='INSERT INTO Players(name, gamertag, email) VALUES (?,?,?)'
-        await db.query(query, [name, gamertag, email]);
+    const getPlayers = async(req, res) =>{
+        try{
+            const [rows] = await db.query('SELECT gamertag, correo, fecha_registro FROM Players');
+            res.status(200).json(rows);
+        }catch(error){
+            console.error(error);
+            res.status(500).json({error: 'Error al obtener la lista de jugadores'});
+        };
 
-        return res.status(201).json({message: 'Jugador registrado exitosamente'});
-    }catch(error){
-        console.error(error);
-        
-
-        if(error.code === 'ER_DUP_ENTRY'){
-            return res.status(400).json({error: 'El Gamertag o el Email no se encuentra disponible'});
-        }
-
-        res.status(500).json({error:'Error en el servidor al registrarte'});
     };
-};
-
-const getPlayers = async(req, res) =>{
-    try{
-        const [rows] = await db.query('SELECT gamertag, email, date_register FROM Players');
-        res.status(200).json(rows);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: 'Error al obtener la lista de jugadores'});
-    };
-
-};
-        // ER_DUP_ENTRY (que es la etiqueta que usa MySQL para decir 'Entrada duplicada')
-        // .code: Es el código o la etiqueta exacta que le puso MySQL al tipo de error que ocurrió.
-module.exports = {registerPlayers, getPlayers};
+            // ER_DUP_ENTRY (que es la etiqueta que usa MySQL para decir 'Entrada duplicada')
+            // .code: Es el código o la etiqueta exacta que le puso MySQL al tipo de error que ocurrió.
+    module.exports = {registerPlayers, getPlayers};
