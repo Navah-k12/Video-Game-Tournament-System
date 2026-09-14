3f5b435bfbaa5fd47ac8f79ed3823361b2f3f51c
Navah-k12 <navahca21@gmail.com>
2026-09-13 23:15:31 -0500

    feat: modifico el db para proteger información sensible

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,20 @@
 
-const mysql = require('mysql2/promise');
 
+
+const mysql = require('mysql2/promise');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '../../.env')}); // Carga las variables del archivo .env
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'nava1234',
-    database: 'SistemaTorneos'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.getConnection()
     .then(connection => {
-        console.log('¡Conectado exitosamente a la base de datos SistemaTorneos!');
+        console.log('¡Conectado exitosamente a la base de datos SistemaTorneos desde .env!');
         connection.release();
     })
     .catch(err => {
@@ -19,3 +22,4 @@ db.getConnection()
     });
 
 module.exports = db;
+
